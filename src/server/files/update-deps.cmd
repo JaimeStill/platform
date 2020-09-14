cb96b098ba64e4bfbaea30747ba596fee9b1fee7
Jaime Still <jpstill85@gmail.com>
2020-09-14 10:46:57 -0400

    update platform dependencies

--- src/server/files/update-deps.cmd
+++ src/server/files/update-deps.cmd
@@ -5,6 +5,10 @@ cd .\dbseeder
 call dotnet add package Microsoft.EntityFrameworkCore.Relational
 call dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 
+echo Updating <%= classify(name) %>.Core dependencies...
+cd ..\<%= classify(name) %>.Core
+call dotnet add package Microsoft.EntityFrameworkCore
+
 echo Updating <%= classify(name) %>.Data dependencies...
 cd ..\<%= classify(name) %>.Data
 call dotnet add package Microsoft.EntityFrameworkCore.SqlServer
